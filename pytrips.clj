d54742291a36b2f00b3e68b642438112c9b1d494
nicholasrussell <nick.f.russell@gmail.com>
2015-08-25 16:22:41 -0500

    Remove comments

--- pytrips.clj
+++ pytrips.clj
@@ -8,15 +8,15 @@
            (f (fn [& y] (apply (x x) y))))))
         (fn [f]
           (fn [a b c]
-            (when (< a n) ; n - 4, n - 2
+            (when (< a n)
               (if (and
                     (= (+ a b c) n)
                     (and (and (< a b) (< b c)) (= (+ (* a a) (* b b)) (* c c))))
                 [a b c]
                 (let [[a b c]
-                        (if (< c n) ; n - 2
+                        (if (< c n)
                           [a b (+ c 1)]
-                          (if (< b n) ; n - 3, n - 1
+                          (if (< b n)
                             [a (+ b 1) (+ b 2)]
                             [(+ a 1) (+ a 2) (+ a 3)]))]
                   (recur a b c))))))) 1 2 3)]

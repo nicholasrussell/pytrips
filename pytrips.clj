2e4d0508ae778350b12642644ce5eb490c9b2c91
nicholasrussell <nick.f.russell@gmail.com>
2015-08-26 07:37:22 -0500

    Use math to help with brute forcing

--- pytrips.clj
+++ pytrips.clj
@@ -1,22 +1,24 @@
-(let 
-  [n 1000
-   [a b c]
-      (((fn [f]
-          ((fn [x]
-           (f (fn [& y] (apply (x x) y))))
-          (fn [x]
-           (f (fn [& y] (apply (x x) y))))))
-        (fn [f]
-          (fn [a b c]
-            (when (< a n)
-              (if (and (= (+ a b c) n) (= (+ (* a a) (* b b)) (* c c)))
-                [a b c]
-                (let [[a b c]
-                        (if (< c n)
-                          [a b (+ c 1)]
-                          (if (< b n)
-                            [a (+ b 1) (+ b 2)]
-                            [(+ a 1) (+ a 2) (+ a 3)]))]
-                  (recur a b c))))))) 1 2 3)]
-  (println a "*" b "*" c "=" (* a b c))
-  (* a b c))
+(defn pytrips [t]
+  (let 
+    [[a b c]
+        (((fn [f]
+            ((fn [x]
+             (f (fn [& y] (apply (x x) y))))
+            (fn [x]
+             (f (fn [& y] (apply (x x) y))))))
+          (fn [f]
+            (fn [n m]
+              (if (> m t)
+                [0 0 0]
+                (let [a (- (* m m) (* n n))
+                      b (* 2 m n)
+                      c (+ (* m m) (* n n))]
+                  (if (and (= (+ a b c) t) (= (+ (* a a) (* b b)) (* c c)))
+                    [a b c]
+                    (if (< n (- m 1))
+                      (recur (+ n 1) m)
+                      (recur 1 (+ m 1))))))))) 1 2)]
+    (println a "*" b "*" c "=" (* a b c))
+    (* a b c)))
+
+(pytrips 1000)

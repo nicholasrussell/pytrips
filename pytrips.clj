a10f05a6715a6700f0ad8f38647243929418c83d
nicholasrussell <nick.f.russell@gmail.com>
2015-08-28 13:08:39 -0500

    Remove now unnecessary let

--- pytrips.clj
+++ pytrips.clj
@@ -1,26 +1,23 @@
 (defn pytrips [t]
   "Returns the three numbers of a Pythagorean triple which add up to t
   (pytrips 1000) -> [375 200 425]"
-  (let 
-    [[a b c]
-        (((fn [f]
-            ((fn [x]
-             (f (fn [& y] (apply (x x) y))))
-            (fn [x]
-             (f (fn [& y] (apply (x x) y))))))
-          (fn [f]
-            (fn [n m]
-              (if (> m t)
-                [0 0 0]
-                (let [a (- (* m m) (* n n))
-                      b (* 2 m n)
-                      c (+ (* m m) (* n n))]
-                  (if (and (= (+ a b c) t) (= (+ (* a a) (* b b)) (* c c)))
-                    [a b c]
-                    (if (< n (- m 1))
-                      (recur (+ n 1) m)
-                      (recur 1 (+ m 1))))))))) 1 2)]
-    [a b c]))
+  (((fn [f]
+      ((fn [x]
+         (f (fn [& y] (apply (x x) y))))
+        (fn [x]
+          (f (fn [& y] (apply (x x) y))))))
+     (fn [f]
+       (fn [n m]
+         (if (> m t)
+           [0 0 0]
+           (let [a (- (* m m) (* n n))
+                 b (* 2 m n)
+                 c (+ (* m m) (* n n))]
+             (if (and (= (+ a b c) t) (= (+ (* a a) (* b b)) (* c c)))
+               [a b c]
+               (if (< n (- m 1))
+                 (recur (+ n 1) m)
+                 (recur 1 (+ m 1))))))))) 1 2))
 
 (let
   [[a b c] (pytrips 1000)
